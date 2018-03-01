6cabed2df4c8899a5ba038b06b9a11c651e46e77
Rob O'Doherty <rob.odoherty@paddypowerbetfair.com>
2018-03-01 21:31:07 +0000

    Adding sample code for rabbit publisher and rabbit consumer

---
+++ src/samples/basicsample/MyPublishingActor.scala
@@ -0,0 +1,31 @@
+
+package com.paddypowerbetfair.rabbitmq.basicsample
+
+import akka.actor.{Actor, ActorLogging, ActorRef, Props, Stash}
+import com.paddypowerbetfair.rabbitmq.{BasicPublish, PublisherChannelActor}
+
+object MyPublishingActor {
+
+  def props: Props = Props(new MyPublishingActor())
+}
+
+
+
+class MyPublishingActor extends Actor with ActorLogging with Stash {
+
+  def receive: Receive = {
+    case PublisherChannelActor(rabbitPublisher) =>
+      unstashAll()
+      context.become(readyToPublish(rabbitPublisher))
+
+    case _ => stash()
+  }
+
+
+  def readyToPublish(publisher: ActorRef): Receive = {
+    case msg: BasicPublish =>
+      publisher ! msg
+      sender() ! s"Message Published - $msg"
+  }
+
+}
